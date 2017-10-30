f0f9f6eab44597b0da9724e8d0c3739e244a3db2
Coinlancer <32292847+Coinlancer@users.noreply.github.com>
2017-10-30 16:56:01 +0200

    Update CoinlancerDealProvider.sol

--- CoinlancerDealProvider.sol
+++ CoinlancerDealProvider.sol
@@ -1,151 +1,140 @@
-pragma solidity ^0.4.11;
-contract owned
-{
-    address public owner;
-
-    function owned()
-    {
-        owner = msg.sender;
-    }
-
-    modifier onlyOwner
-    {
-        if (msg.sender != owner) throw;
-        _;
-    }
-
-    function transferOwnership(address newOwner) onlyOwner
-    {
-        owner = newOwner;
-    }
-}
-
-contract tokenRecipient { function receiveApproval(address _from, uint256 _value, address _token, bytes _extraData); }
-
-contract token
-{
-    /* Public variables of the token */
-    string public name;
-    string public symbol;
-    uint8 public decimals;
-    uint256 public totalSupply;
-
-    /* This creates an array with all balances */
-    mapping (address => uint256) public balanceOf;
-    mapping (address => mapping (address => uint256)) public allowance;
-
-    /* This generates a public event on the blockchain that will notify clients */
-    event Transfer(address indexed from, address indexed to, uint256 value);
-
-    /* Initializes contract with initial supply tokens to the creator of the contract */
-    function token
-    (
-        uint256 initialSupply,
-        string tokenName,
-        uint8 decimalUnits,
-        string tokenSymbol
-    )
-    {
-        balanceOf[msg.sender] = initialSupply;              // Give the creator all initial tokens
-        totalSupply = initialSupply;                        // Update total supply
-        name = tokenName;                                   // Set the name for display purposes
-        symbol = tokenSymbol;                               // Set the symbol for display purposes
-        decimals = decimalUnits;                            // Amount of decimals for display purposes
-    }
-
-    /* Send coins */
-    function transfer(address _to, uint256 _value)
-    {
-        if (balanceOf[msg.sender] < _value) throw;           // Check if the sender has enough
-        if (balanceOf[_to] + _value < balanceOf[_to]) throw; // Check for overflows
-        balanceOf[msg.sender] -= _value;                     // Subtract from the sender
-        balanceOf[_to] += _value;                            // Add the same to the recipient
-        Transfer(msg.sender, _to, _value);                   // Notify anyone listening that this transfer took place
-    }
-
-    /* Allow another contract to spend some tokens in your behalf */
-    function approve(address _spender, uint256 _value) returns (bool success)
-    {
-        allowance[msg.sender][_spender] = _value;
-        return true;
-    }
-    
-    /* A contract attempts to get the coins */
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        if (balanceOf[_from] < _value) throw;                 // Check if the sender has enough
-        if (balanceOf[_to] + _value < balanceOf[_to]) throw;  // Check for overflows
-        if (_value > allowance[_from][msg.sender]) throw;   // Check allowance
-        balanceOf[_from] -= _value;                          // Subtract from the sender
-        balanceOf[_to] += _value;                            // Add the same to the recipient
-        allowance[_from][msg.sender] -= _value;
-        Transfer(_from, _to, _value);
-        return true;
-    }
-
-    /* This unnamed function is called whenever someone tries to send ether to it */
-    function ()
-    {
-    //    throw;     // Prevents accidental sending of ether
-    }
-}
-
-contract StandardToken is owned, token
-{
-    
-    /* Initializes contract with initial supply tokens to the creator of the contract */
-    function StandardToken(
-        uint256 initialSupply,
-        string tokenName,
-        uint8 decimalUnits,
-        string tokenSymbol
-    ) token (initialSupply, tokenName, decimalUnits, tokenSymbol) {}
+pragma solidity 0.4.13;
+  
+ // ----------------------------------------------------------------------------------------------
+ // Coinlancer fixed supply token contract
+ // Enjoy. (c) etype 2017. The MIT Licence.
+ // ----------------------------------------------------------------------------------------------
+  
+ // ERC Token Standard #20 Interface
+ // https://github.com/ethereum/EIPs/issues/20
+ contract ERC20Interface {
+     // Get the total token supply
+     function totalSupply() constant returns (uint256 totalSupply);
+  
+     // Get the account balance of another account with address _owner
+     function balanceOf(address _owner) constant returns (uint256 balance);
+  
+     // Send _value amount of tokens to address _to
+     function transfer(address _to, uint256 _value) returns (bool success);
+  
+     // Send _value amount of tokens from address _from to address _to
+     function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
+  
+     // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
+     // If this function is called again it overwrites the current allowance with _value.
+     // this function is required for some DEX functionality
+     function approve(address _spender, uint256 _value) returns (bool success);
+  
+     // Returns the amount which _spender is still allowed to withdraw from _owner
+     function allowance(address _owner, address _spender) constant returns (uint256 remaining);
+  
+     // Triggered when tokens are transferred.
+     event Transfer(address indexed _from, address indexed _to, uint256 _value);
+  
+     // Triggered whenever approve(address _spender, uint256 _value) is called.
+     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+ }
+  
+ contract Coinlancer is ERC20Interface {
+     string public constant symbol = "CL";
+     string public constant name = "Coinlancer";
+     uint8 public constant decimals = 18;
+     uint256 _totalSupply = 300000000000000000000000000;
+     
+     // Owner of this contract
+     address public owner;
+  
+     // Balances for each account
+     mapping(address => uint256) balances;
+  
+     // Owner of account approves the transfer of an amount to another account
+     mapping(address => mapping (address => uint256)) allowed;
+  
+     // Functions with this modifier can only be executed by the owner
+     modifier onlyOwner() {
+         require(msg.sender != owner); {
+             
+          }
+          _;
+      }
    
-    /* Send coins */
-    function transfer(address _to, uint256 _value)
-    {
-        if (balanceOf[msg.sender] < _value) throw;           // Check if the sender has enough
-        if (balanceOf[_to] + _value < balanceOf[_to]) throw; // Check for overflows
-        balanceOf[msg.sender] -= _value;                     // Subtract from the sender
-        balanceOf[_to] += _value;                            // Add the same to the recipient
-        Transfer(msg.sender, _to, _value);                   // Notify anyone listening that this transfer took place
-    }
-    
-    /* A contract attempts to get the coins */
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        if (balanceOf[_from] < _value) throw;                 // Check if the sender has enough
-        if (balanceOf[_to] + _value < balanceOf[_to]) throw;  // Check for overflows
-        if (_value > allowance[_from][msg.sender]) throw;   // Check allowance
-        balanceOf[_from] -= _value;                          // Subtract from the sender
-        balanceOf[_to] += _value;                            // Add the same to the recipient
-        allowance[_from][msg.sender] -= _value;
-        Transfer(_from, _to, _value);
-        return true;
-    }
-    
+      // Constructor
+      function Coinlancer() {
+          owner = msg.sender;
+          balances[owner] = _totalSupply;
+      }
+  
+      function totalSupply() constant returns (uint256 totalSupply) {
+         totalSupply = _totalSupply;
+      }
+  
+      // What is the balance of a particular account?
+      function balanceOf(address _owner) constant returns (uint256 balance) {
+         return balances[_owner];
+      }
+   
+      // Transfer the balance from owner's account to another account
+      function transfer(address _to, uint256 _amount) returns (bool success) {
+         if (balances[msg.sender] >= _amount 
+              && _amount > 0
+              && balances[_to] + _amount > balances[_to]) {
+              balances[msg.sender] -= _amount;
+              balances[_to] += _amount;
+              Transfer(msg.sender, _to, _amount);
+              return true;
+          } else {
+              return false;
+         }
+      }
+   
+      // Send _value amount of tokens from address _from to address _to
+      // The transferFrom method is used for a withdraw workflow, allowing contracts to send
+      // tokens on your behalf, for example to "deposit" to a contract address and/or to charge
+      // fees in sub-currencies; the command should fail unless the _from account has
+      // deliberately authorized the sender of the message via some mechanism; we propose
+      // these standardized APIs for approval:
+      function transferFrom(
+          address _from,
+          address _to,
+         uint256 _amount
+    ) returns (bool success) {
+       if (balances[_from] >= _amount
+            && allowed[_from][msg.sender] >= _amount
+           && _amount > 0
+            && balances[_to] + _amount > balances[_to]) {
+           balances[_from] -= _amount;
+           allowed[_from][msg.sender] -= _amount;
+            balances[_to] += _amount;
+             Transfer(_from, _to, _amount);
+             return true;
+        } else {
+            return false;
+         }
+     }
+  
+    // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
+     // If this function is called again it overwrites the current allowance with _value.
+     function approve(address _spender, uint256 _amount) returns (bool success) {
+         allowed[msg.sender][_spender] = _amount;
+         Approval(msg.sender, _spender, _amount);
+         return true;
+     }
+  
+     function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+         return allowed[_owner][_spender];
+     }
 }
 
 contract DealProvider
 {
-    StandardToken public token;
+    Coinlancer public token;
     
     address public feeAccount;
-    //= 0xdd870fa1b7c4700f2bd7f44238821c26f7392148;
-    address public escrow;
-    //= 0x14723a09acff6d2a60dcdf7aa4aff308fddc160c;
+    address public owner;
 
-    
-    /* 
-    address public client = 0x4b0897b0513fdc7c541b6d9d7e929c4e5364d2db;
-    address public executor = 0x583031d1113ad414f02576bd6afabfb302140225;
-    */
-    
-    //mapping (uint256 => uint256) public payed_amounts;
     mapping (uint256 => address[2]) public addresses;
     mapping (uint256 => uint256) public comissions;
     mapping (uint256 => uint256) public step_payments;
-    
-    address public owner;
-
     
     modifier onlyOwner
     {
@@ -155,7 +144,7 @@ contract DealProvider
     
     function DealProvider(address token_address)
     {
-        token = StandardToken(token_address);
+        token = Coinlancer(token_address);
         owner = msg.sender;
     }
     
@@ -163,16 +152,6 @@ contract DealProvider
     function transferOwnership(address newOwner) onlyOwner
     {
         owner = newOwner;
-    }
-     
-    function setEscrowAccount() onlyOwner
-    {
-        escrow = this;
-    }
-   
-    function getEscrow() returns (address)
-    {
-        return escrow;
     }
     
     function setFeeAccount(address new_feeAccount) onlyOwner
@@ -191,21 +170,12 @@ contract DealProvider
         step_payments[id] = amount;
     }
     
-   /* function setPayedAmount(uint256 id, uint256 amount, uint256 fee) onlyOwner
-    {
-        payed_amounts[id] = amount;
-        comissions[id] = fee;
-    }*/
-    
+   
     function setComission(uint256 id, uint256 fee) onlyOwner
     {
         comissions[id] = fee;
     }
     
-    /*function withdrawFee(uint256 id) onlyOwner
-    {
-        token.transferFrom(this, feeAccount, comissions[id]);
-    }*/
     
     function stepTransferToEscrow(uint256 id) onlyOwner
     {
