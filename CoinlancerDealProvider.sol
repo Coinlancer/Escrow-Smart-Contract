43e20e02df7f1c4e1ca157239d29e8486b16faf6
Coinlancer <32292847+Coinlancer@users.noreply.github.com>
2017-11-07 17:58:16 +0200

    Update CoinlancerDealProvider.sol

--- CoinlancerDealProvider.sol
+++ CoinlancerDealProvider.sol
@@ -1,81 +1,79 @@
-pragma solidity 0.4.13;
-  
  // ----------------------------------------------------------------------------------------------
  // Coinlancer fixed supply token contract
  // Enjoy. (c) etype 2017. The MIT Licence.
  // ----------------------------------------------------------------------------------------------
-  
+
  // ERC Token Standard #20 Interface
  // https://github.com/ethereum/EIPs/issues/20
  contract ERC20Interface {
      // Get the total token supply
      function totalSupply() constant returns (uint256 totalSupply);
-  
+
      // Get the account balance of another account with address _owner
      function balanceOf(address _owner) constant returns (uint256 balance);
-  
+
      // Send _value amount of tokens to address _to
      function transfer(address _to, uint256 _value) returns (bool success);
-  
+
      // Send _value amount of tokens from address _from to address _to
      function transferFrom(address _from, address _to, uint256 _value) returns (bool success);
-  
+
      // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
      // If this function is called again it overwrites the current allowance with _value.
      // this function is required for some DEX functionality
      function approve(address _spender, uint256 _value) returns (bool success);
-  
+
      // Returns the amount which _spender is still allowed to withdraw from _owner
      function allowance(address _owner, address _spender) constant returns (uint256 remaining);
-  
+
      // Triggered when tokens are transferred.
      event Transfer(address indexed _from, address indexed _to, uint256 _value);
-  
+
      // Triggered whenever approve(address _spender, uint256 _value) is called.
      event Approval(address indexed _owner, address indexed _spender, uint256 _value);
  }
-  
+
  contract Coinlancer is ERC20Interface {
      string public constant symbol = "CL";
      string public constant name = "Coinlancer";
      uint8 public constant decimals = 18;
      uint256 _totalSupply = 300000000000000000000000000;
-     
+
      // Owner of this contract
      address public owner;
-  
+
      // Balances for each account
      mapping(address => uint256) balances;
-  
+
      // Owner of account approves the transfer of an amount to another account
      mapping(address => mapping (address => uint256)) allowed;
-  
+
      // Functions with this modifier can only be executed by the owner
      modifier onlyOwner() {
          require(msg.sender != owner); {
-             
+
           }
           _;
       }
-   
+
       // Constructor
       function Coinlancer() {
           owner = msg.sender;
           balances[owner] = _totalSupply;
       }
-  
+
       function totalSupply() constant returns (uint256 totalSupply) {
          totalSupply = _totalSupply;
       }
-  
+
       // What is the balance of a particular account?
       function balanceOf(address _owner) constant returns (uint256 balance) {
          return balances[_owner];
       }
-   
+
       // Transfer the balance from owner's account to another account
       function transfer(address _to, uint256 _amount) returns (bool success) {
-         if (balances[msg.sender] >= _amount 
+         if (balances[msg.sender] >= _amount
               && _amount > 0
               && balances[_to] + _amount > balances[_to]) {
               balances[msg.sender] -= _amount;
@@ -86,7 +84,7 @@ pragma solidity 0.4.13;
               return false;
          }
       }
-   
+
       // Send _value amount of tokens from address _from to address _to
       // The transferFrom method is used for a withdraw workflow, allowing contracts to send
       // tokens on your behalf, for example to "deposit" to a contract address and/or to charge
@@ -111,7 +109,7 @@ pragma solidity 0.4.13;
             return false;
          }
      }
-  
+
     // Allow _spender to withdraw from your account, multiple times, up to the _value amount.
      // If this function is called again it overwrites the current allowance with _value.
      function approve(address _spender, uint256 _amount) returns (bool success) {
@@ -119,85 +117,99 @@ pragma solidity 0.4.13;
          Approval(msg.sender, _spender, _amount);
          return true;
      }
-  
+
      function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
          return allowed[_owner][_spender];
      }
 }
 
+
 contract Escrow
 {
     Coinlancer public token;
-    
+
     address public feeAccount;
     address public founder;
     address public owner;
-    
-    uint256 fee_mul = 3;
-    //uint256 fee_den = 100;
 
-    mapping (uint256 => address[2]) public addresses;
-    mapping (uint256 => uint256) public payments;
-    
+    uint fee_mul = 3;
+    //uint fee_den = 100;
+
+    struct Step {
+        address from;
+        address to;
+        uint    amount;
+    }
+
+    mapping (uint => Step) public steps;
+
     modifier onlyOwner
     {
-        if (msg.sender != owner) throw;
+        require(msg.sender == owner);
         _;
     }
-    
+
     function Escrow(address token_address)
     {
         token = Coinlancer(token_address);
         founder = msg.sender;
         owner = msg.sender;
     }
-    
+
 
     function transferOwnership(address newOwner)
     {
-        if (msg.sender != founder) throw;
+        require(msg.sender == founder);
         owner = newOwner;
     }
-    
-    function setFeeAccount (address new_feeAccount) onlyOwner
+
+    function setFeeAccount (address new_account) onlyOwner
     {
-        feeAccount = new_feeAccount;
+        feeAccount = new_account;
     }
-    
-    function setFee (uint256 fee) onlyOwner
+
+    function setFee (uint fee) onlyOwner
     {
-        if (fee > 100) throw;
+        require(fee >= 0 && fee < 100);
         fee_mul = fee;
     }
-    
-    function deposit (uint256 step_id, address from, address to, uint256 amount) onlyOwner
+
+    function deposit (uint step_id, address from, address to, uint amount) onlyOwner
     {
-        //uint256 fee;
-        uint256 fee = (amount * fee_mul) / 100;
-        if (token.balanceOf(from) < amount + fee) throw;
-        if (feeAccount == 0x0) throw;
-        if (payments[step_id] != 0) throw;
-        addresses[step_id][0] = from;
-        addresses[step_id][1] = to;
-        payments[step_id] = amount;
-        token.transferFrom(addresses[step_id][0], this, payments[step_id]);    //transfer to escrow
-        token.transferFrom(addresses[step_id][0], feeAccount, fee); //withdraw fee
+        require(from != address(0));
+        require(to != address(0));
+        require(amount > 0);
+        require(feeAccount != address(0));
+
+        uint fee = (amount * fee_mul) / 100;
+        require(token.balanceOf(from) >= amount + fee);
+
+        Step storage step = steps[step_id];
+        require(step.from == address(0));
+
+        step.from = from;
+        step.to = to;
+        step.amount = amount;
+
+        token.transferFrom(step.from, this, step.amount);    //transfer to escrow
+        token.transferFrom(step.from, feeAccount, fee); //withdraw fee
     }
-    
-    function pay (uint256 step_id) onlyOwner
+
+    function pay (uint step_id) onlyOwner
     {
-        if (payments[step_id] == 0) throw;
-        token.transfer(addresses[step_id][1], payments[step_id]);
-        delete payments[step_id];
-        delete addresses[step_id];
+        Step step = steps[step_id];
+        require(step.from != address(0));
+
+        token.transfer(step.to, step.amount);
+        delete steps[step_id];
     }
-    
-    function refund (uint256 step_id) onlyOwner
+
+    function refund (uint step_id) onlyOwner
     {
-        if (payments[step_id] == 0) throw;
-        token.transfer(addresses[step_id][0], payments[step_id]);
-        delete payments[step_id];
-        delete addresses[step_id];
+        Step step = steps[step_id];
+        require(step.from != address(0));
+
+        token.transfer(step.from, step.amount);
+        delete steps[step_id];
     }
-    
 }
